7c18b976a1cb2c380ed9f9f389819eb20ee9cbd5
Khusainov779 <khusainov779@gmail.com>
2021-02-10 14:53:26 +0500

    feat: add portfolio page with hero slider

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 import '@babel/polyfill';
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
@@ -8,4 +9,40 @@ window.$ = $;
 window.jQuery = $;
 
 require('ninelines-ua-parser');
+
+$(document).ready(() => {
+	$(window).scroll(() => {
+		if ($(document).scrollTop() > 200) {
+			$('.about__aside').addClass('about__aside--is-visible');
+			$('.about__section').addClass('about__section--is-visible');
+		} else {
+			$('.about__aside').removeClass('about__aside--is-visible');
+			$('.about__section').removeClass('about__section--is-visible');
+		}
+	});
+});
+
+$(document).ready(() => {
+	$(window).scroll(() => {
+		if ($(document).scrollTop() > 900) {
+			$('.home-portfolio__list--item').addClass('portfolio__list--is-visible');
+		} else {
+			$('.home-portfolio__list--item').removeClass('portfolio__list--is-visible');
+		}
+	});
+});
+
+const panels = document.querySelectorAll('.portfolio-hero__list--item');
+
+function checkIt() {
+	this.classList.toggle('open');
+}
+function checkEnd(e) {
+	if (e.propertyName.includes('flex')) {
+		this.classList.toggle('open-active');
+	}
+}
+
+panels.forEach((panel) => panel.addEventListener('click', checkIt));
+panels.forEach((panel) => panel.addEventListener('transitionend', checkEnd));
 
